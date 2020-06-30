b8c7d67664e75fed44db84ce7406ec1e715c68a3
“huwuqiang” <928536927@qq.com>
2020-07-01 01:09:51 +0800

    添加了一行代码

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,5 +3,6 @@ public  class Demo extends AbstractUser{
     public static void main(String[] args) {
         Demo demo = new Demo();
         demo.haha();
+        System.out.println("q123");
     }
 }
